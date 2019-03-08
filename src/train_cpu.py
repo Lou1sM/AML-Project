6c2676043d858624c43867847e13a0873d86b0e3
Vladimir Barvinko <v.p.barvinko@student.tue.nl>
2019-03-08 17:18:58 +0000

    remove tmp writing

--- src/train_cpu.py
+++ src/train_cpu.py
@@ -1,115 +1,202 @@
 import tensorflow as tf
 import highway_max_out
-import andrei_encoder_script
+import encoder
 import ciprian_data_prep_script
+import tfrecord_converter
+import argparse
+import time
+import datetime
+import os
+import utils
+from tensorflow.python.client import timeline
 
-# Global variables
-# We may want to move these to an object and pass it to components
+start_time = time.time()
 
-# Learning Parameters
-num_epochs = 100
-num_batches = 32
-learning_rate = 1e-3
-dropout = 0
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-# Architecture Parameters
-hidden_state_size = 200
-num_decoding_lstm_layers = 1  # assumed to be 1 in paper
-
-# Whatever the format of the data preparation, the output will
-# be a document-length sequence of glove word vectors, and a
-# question-length sequence of the same. Ideally it these would
-# be generator objects with the faster tf.data.Dataset, but
-# another format is ok if that's all that's possible
-#
-# For example:
-input_d_vecs, input_q_vecs, ground_truth_labels = ciprian_data_prep_script.get_data()
+import logging
+logging.getLogger('tensorflow').setLevel(50)
 
 
-# Encode into the matrix U, using notation from the paper
-# The output should be of shape [2*hidden_size, document_length]
-encoded = andrei_encoder_script.encoder(input_d_vecs, input_q_vecs)
+parser = argparse.ArgumentParser()
+parser.add_argument("--num_epochs", default=1, type=int, help="Number of epochs to train for")
+parser.add_argument("--restore", action="store_true", default=False, help="Whether to restore weights from previous run")
+parser.add_argument("--num_units", default=200, type=int,
+	help="Number of recurrent units for the first lstm, which is deteriministic and is only used in both training and testing")
+parser.add_argument("--test", "-t", default=False, action="store_true", help="Whether to run in test mode")
+parser.add_argument("--batch_size", default=10, type=int, help="Size of each training batch")
+parser.add_argument("--dataset", choices=["SQuAD"],default="SQuAD", type=str, help="Dataset to train and evaluate on")
+parser.add_argument("--hidden_size", default=200, type=int, help="Size of the hidden state")
+parser.add_argument("--keep_prob", default=1, type=float, help="Keep probability for question and document encodings.")
+parser.add_argument("--learning_rate", default=1e-3, type=float, help="Learning rate.")
+parser.add_argument("--short_test", "-s", default=False, action="store_true", help="Whether to run in short test mode")
 
-# Create and initialize decoding LSTM
-decoding_lstm = tf.contrib.rnn.LSTMCell(num_units = hidden_state_size)
-h = decoding_lstm.zer_state(num_batches, dtype=tf.float32)
+ARGS = parser.parse_args()
 
+"""
+with tf.name_scope("data_prep"):
+    input_d_vecs, input_q_vecs, ground_truth_labels, documents_lengths, questions_lengths = ciprian_data_prep_script.get_data()
+    print("In train.py: get_data finished.")
+    start_l = list(map(lambda x: x[0], ground_truth_labels))
+    end_l = list(map(lambda x: x[1], ground_truth_labels))
+    d = tf.placeholder(tf.float64, [ARGS.batch_size, len(input_d_vecs[0]), len(input_d_vecs[0][0])])
+    q = tf.placeholder(tf.float64, [ARGS.batch_size, len(input_q_vecs[0]), len(input_q_vecs[0][0])])
+    starting_labels = tf.placeholder(tf.int64, [ARGS.batch_size])
+    ending_labels = tf.placeholder(tf.int64, [ARGS.batch_size])
+    doc_l = tf.placeholder(tf.int64, [ARGS.batch_size])
+    que_l = tf.placeholder(tf.int64, [ARGS.batch_size])
+"""
 
-# Get lstm_dec hidden state values from U in order to make the
-# first guess for start- and end-points
-# Output should be of shape [2*hidden_size]
-# Each guess is based on the previous guess so it seems we need
-# to start with some random guess, eg first and last document words
+dataset = tfrecord_converter.read_tfrecords(file_names=('test.tfrecord'))
+#dataset = dataset.flat_map(lambda x: tf.data.Dataset.from_tensorslices(x))
+dataset = dataset.batch(ARGS.batch_size)
+iter_ = dataset.make_initializable_iterator()
 
-u_s = encoded[0, :, :]  # Dummy guess start point
-u_e = encoded[-1, :, :]  # Dummy guess end point
+data_dict = iter_.get_next()
+d = data_dict['D']
+q = data_dict['Q']
+a = data_dict['A']
+doc_l = data_dict['DL']
+que_l = data_dict['QL']
 
-for i in range(4):
-
-    usue = tf.concat([u_s, u_e], axis=1)
-    # Here, "h" is the concatenation of cell output and cell state
-    # (h_t and C_t in colah.github.io/posts/2015-08-Understanding-LSTMs/ )
-    # decoding_lstm returns (h_t, (h_t, C_t))
-    cell_output, h = decoding_lstm(inputs=usue, state=h)
-
-    # Make the first estimation of start- and end-points. Define the
-    # set of graph nodes for the HMN twice in two different namespaces
-    # so that two copies are created and they can be trained separately
-    with tf.variable_scope('start_estimator'):
-        first_guess_start_points = highway_max_out.HMM(
-            current_words=encoded,
-            lstm_hidden_state=h,
-            prev_start_point_guess=u_s,
-            prev_end_point_guess=u_e
-        )
-
-    with tf.variable_scope('end_estimator'):
-        first_guess_end_points = highway_max_out.HMM(
-            current_words=encoded,
-            lstm_hidden_state=h,
-            prev_start_point_guess=u_s,
-            prev_end_point_guess=u_e
-        )
-
-    s = tf.argmax(first_guess_start_points)
-    u_s = encoded[s, :, :]
-    e = tf.argmax(first_guess_start_points)
-    u_e = encoded[e, :, :]
-
-    # Define loss and optimizer, each guess contributes to loss,
-    # even the very first
-    s_loss = tf.nn.softmax_cross_entropy_with_logits_v2(
-        labels=ground_truth_labels[0],
-        logits=first_guess_start_points
+with tf.name_scope("encoder"):
+    encoded = encoder.encoder(
+        document=d,
+        question=q,
+        documents_lengths=doc_l,
+        questions_lengths=que_l,
+        hyperparameters=ARGS
     )
+    # Create single nodes for labels
 
-    e_loss = tf.nn.softmax_cross_entropy_with_logits_v2(
-        labels=ground_truth_labels[1],
-        logits=first_guess_end_points
-    )
+    start_labels = tf.one_hot(a[:,0], 766)
+    end_labels = tf.one_hot(a[:,1], 766)
 
-    iteration_loss = s_loss + e_loss
+with tf.name_scope("decoder"):
+    # Static tensor to be re-used in main loop iterations
+    batch_indices = tf.range(start=0, limit=ARGS.batch_size, dtype=tf.int32)
 
-    loss = iteration_loss if i == 0 else loss + iteration_loss
+    # Create and initialize decoding LSTM
+    decoding_lstm = tf.contrib.cudnn_rnn.CudnnLSTM(
+        num_layers=1,
+        num_units=ARGS.hidden_size)
 
-# Set up learning process
-optimizer = tf.train.AdamOptimizer(learning_rate)  # They don't give learning rate!
-train_step = optimizer.minimize(loss)
+    # first guess for start- and end-points
+    u_s = tf.zeros([ARGS.batch_size, 2 * ARGS.hidden_size])  # Dummy guess start point
+    u_e = tf.zeros([ARGS.batch_size, 2 * ARGS.hidden_size])  # Dummy guess end point
+
+    with tf.name_scope("decoding_loop"):
+        for i in range(4):
+            # LSTM input is concatenation of previous guesses
+            usue = tf.concat([u_s, u_e], axis=1)
+            # CudnnLSTM expects input with shape [time_len, batch_size, input_size]
+            # As the inputs across time depend on the previous outputs, we simply set
+            # time_len = 0 and repeat the calculation multiple times while setting
+            # the 'initial' state to the previous output. CudnnLSTMs output is
+            # similarly time-major (has a first dimension that spans time).
+
+            # an initial_state of None corresponds to initialisation with zeroes
+            with tf.variable_scope("decoding_lstm", reuse=tf.AUTO_REUSE):
+                lstm_output, h = decoding_lstm(
+                    inputs=tf.expand_dims(input=usue, axis=0),
+                    initial_state=None if i == 0 else h,
+                    training=True)
+                lstm_output_reshaped = tf.squeeze(lstm_output, [0])
+
+            # Make an estimation of start- and end-points. Define the
+            # set of graph nodes for the HMN twice in two different namespaces
+            # so that two copies are created and they can be trained separately
+
+            with tf.variable_scope('start_estimator'):
+                alphas = highway_max_out.HMN(
+                    current_words=encoded,
+                    lstm_hidden_state=lstm_output_reshaped,
+                    prev_start_point_guess=u_s,
+                    prev_end_point_guess=u_e,
+                    hyperparameters=ARGS,
+                    name="HMN_start"
+                )
+
+            with tf.variable_scope('end_estimator'):
+                betas = highway_max_out.HMN(
+                    current_words=encoded,
+                    lstm_hidden_state=lstm_output_reshaped,
+                    prev_start_point_guess=u_s,
+                    prev_end_point_guess=u_e,
+                    hyperparameters=ARGS,
+                    name="HMN_end"
+                )
+
+            s = tf.argmax(alphas, axis=1, output_type=tf.int32)
+            s_indices = tf.transpose(tf.stack([s, batch_indices]))
+            u_s = tf.gather_nd(encoded, s_indices)
+
+            e = tf.argmax(betas, axis=1, output_type=tf.int32)
+            e_indices = tf.transpose(tf.stack([e, batch_indices]))
+            u_e = tf.gather_nd(encoded, e_indices)
+
+            # Each guess contributes to loss,
+            # even the very first
+            s_loss = tf.nn.softmax_cross_entropy_with_logits_v2(
+                labels=start_labels,
+                logits=alphas
+            )
+            e_loss = tf.nn.softmax_cross_entropy_with_logits_v2(
+                labels=end_labels,
+                logits=betas
+            )
+
+            with tf.name_scope("iteration_" + str(i) + "_loss"):
+                iteration_loss = s_loss + e_loss
+                tf.summary.scalar('loss', tf.reduce_mean(iteration_loss))
+
+            loss = iteration_loss if i == 0 else loss + iteration_loss
+
+mean_loss = tf.reduce_mean(loss)
+tf.summary.scalar('total_loss', mean_loss)
+optimizer = tf.train.AdamOptimizer(ARGS.learning_rate)
+train_step = optimizer.minimize(mean_loss)
 
 # For Tensorboard
-writer = tf.summary.FileWriter("summaries/")
+merged = tf.summary.merge_all()
+summaryDirectory = "summaries/start_" + str(datetime.datetime.now())
+summaryDirectory = summaryDirectory.replace('.', '_').replace(':', '-').replace(' ', '_')
+tf.gfile.MkDir(summaryDirectory)
+writer = tf.summary.FileWriter(summaryDirectory)
 writer.add_graph(tf.get_default_graph())
 
+saver = tf.train.Saver()
 
-# Define the session, which will use default_graph, ie the
-# graph that contains all the nodes defined in the current
-# script (I think that's what default_graph is anyway)
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+    # add additional options to trace the session execution
+    options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+    run_metadata = tf.RunMetadata()
+    sess.run(tf.global_variables_initializer(), options=options, run_metadata=run_metadata)
 
-    # This bit will depend on data format, we can also
-    # add summaries with 'writer' every so often, for
-    # tensorboard loss visualization
-    for _ in range(num_epochs):
-        for _ in range(num_batches):
-            sess.run(train_step)
+    # Create the Timeline object, and write it to a json file
+    fetched_timeline = timeline.Timeline(run_metadata.step_stats)
+    chrome_trace = fetched_timeline.generate_chrome_trace_format()
+    with open('timeline_01.json', 'w') as f:
+        f.write(chrome_trace)
+
+
+    #sess.run(tf.global_variables_initializer())
+    train_start_time = time.time()
+    print("Time elapsed from beginning until right before starting train is: ", utils.time_format(train_start_time - start_time))
+    for i in range(ARGS.num_epochs):
+        sess.run(iter_.initializer)
+        while True:
+            try:
+                summary, _, loss_val = sess.run([merged, train_step, mean_loss])
+                break
+            except tf.errors.OutOfRangeError:
+                writer.add_summary(summary, i)
+                break
+
+    train_end_time = time.time()
+
+    print("Total training time (without data reading): ", utils.time_format(train_end_time - train_start_time))
+
+    #save_path = saver.save(sess, "/tmp/model.ckpt")
+    #print("Model saved in path: %s" % save_path)
+
