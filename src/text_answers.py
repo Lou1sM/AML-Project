0b35c63c8d18eaaa3c4c9c2242dd8288a1b30e97
andrei ilie <andreii1@AML5.tgatqqmujg0unhsmxttospjtdf.zx.internal.cloudapp.net>
2019-04-06 15:51:13 +0000

    Compute EM tokenized strings score in text_answers script

--- src/text_answers.py
+++ src/text_answers.py
@@ -4,7 +4,7 @@ import nltk
 import datetime
 
 dev_json_filename = 'data/squad/dev-v1.1.json'
-default_predicted_dev_json_filename = 'predictions.json'
+default_predicted_dev_json_filename = 'predictions_epoch_2.json'
 
 default_additional_text = ''
 
@@ -18,10 +18,13 @@ def load_data(filename):
         data = json.loads(data_file.read())
     return data
 
-count_questions = 0
+total_count = 0.1
+correct_predictions = 0.1
 
 def process_actual_answers(predicted_dev_json_filename = default_predicted_dev_json_filename, additional_text = default_additional_text):
-    global count_questions
+    global total_count
+    global correct_predictions
+
     actual_answers_data = load_data(dev_json_filename)
     qid_to_answers = {}
     qid_to_question = {}
@@ -38,7 +41,6 @@ def process_actual_answers(predicted_dev_json_filename = default_predicted_dev_j
         qid_to_answers[qid] = list(map(lambda x: x['text'], dp[0]['answers']))
         qid_to_question[qid] = dp[0]['question']
         qid_to_paragraph[qid] = dp[1]
-        count_questions += 1
 
     predicted_answers_data = load_data(predicted_dev_json_filename)
     epoch = predicted_answers_data['epoch']
@@ -47,7 +49,9 @@ def process_actual_answers(predicted_dev_json_filename = default_predicted_dev_j
     out_file.write("Extra data: " + str(additional_text) + "\n\n\n")
 
     for pred in predictions:
-        pred_qid = pred['qid']
+        total_count += 1.0
+
+        pred_qid = pred['id']
         pred_start = pred['start']
         pred_end = pred['end']
 
@@ -70,6 +74,21 @@ def process_actual_answers(predicted_dev_json_filename = default_predicted_dev_j
         out_file.write("\n_______________________________________________________\n")
         out_file.flush()
 
+        ok = 0
+        for correct_answer in correct_answers:
+        	tokenized_correct_answer = nltk.word_tokenize(correct_answer)
+        	if(tokenized_correct_answer == pred_answer):
+        		ok = 1
+
+        if(ok == 1):
+        	correct_predictions += 1.0
+
+
+    print("EM SCORE: ", correct_predictions / total_count)
+    out_file.write("Correct count: " + str(correct_predictions) + "\n")
+    out_file.write("Total count: " + str(total_count) + "\n")
+    out_file.write("EM SCORE: " + str(correct_predictions / total_count))
+    out_file.write("\n")
     out_file.close()
 
 
